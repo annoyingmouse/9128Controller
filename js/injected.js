d0bc44d393ad21ae5e7b85a4e9ba5616ef742b26
Dominic Myers <annoyingmouse@gmail.com>
2020-02-14 15:00:18 +0000

    Initial commit

---
+++ js/injected.js
@@ -0,0 +1,29 @@
+var port = chrome.runtime.connect({
+    name: "9128Controller"
+});
+var checkExist = setInterval(function () {
+    if (document.getElementById('playButton')) {
+        console.log(document.getElementById('playButton'))
+        var playerButton = document.getElementById('playButton')
+        var observer = new WebKitMutationObserver(function (mutations) {
+            getStatus()
+        });
+
+        function getStatus() {
+            port.postMessage({
+                player: playerButton.classList.contains('icon-playerstop') ? 1 : 2
+            });
+        }
+
+        observer.observe(playerButton, {
+            attributes: true,
+            subtree: false
+        });
+        port.onMessage.addListener(function (msg) {
+            if (msg.command) {
+                playerButton.click();
+            }
+        });
+        clearInterval(checkExist);
+    }
+}, 1000);
